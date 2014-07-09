9418cc05132ba7e6f0c5047660d70497ed28dcad
Kazuho Oku <kazuhooku@gmail.com>
2014-07-09 12:19:20 +0900

    Revert "implement orderby"
    This reverts commit 97cfbdb7783081bff7f03d4ff83831df8363af98.

--- net/examp1e/picoorm/LongPredicate.java
+++ net/examp1e/picoorm/LongPredicate.java
@@ -1,19 +1,19 @@
 package net.examp1e.picoorm;
 
-public class LongPredicate<Row extends AbstractRow, RowOrder> {
-	final TableDefinition<Row, RowOrder> tableDefinition;
+public class LongPredicate<Row extends AbstractRow> {
+	final TableDefinition<Row> tableDefinition;
 	String fieldName;
-	public LongPredicate(TableDefinition<Row, RowOrder> tableDefinition, String fieldName) {
+	public LongPredicate(TableDefinition<Row> tableDefinition, String fieldName) {
 		this.tableDefinition = tableDefinition;
 		this.fieldName = fieldName;
 	}
-	public Condition<Row, RowOrder> is(long x) {
-		Condition<Row, RowOrder> c = new Condition<Row, RowOrder>(this.tableDefinition, this.fieldName + "=?");
+	public Condition<Row> is(long x) {
+		Condition<Row> c = new Condition<Row>(this.tableDefinition, this.fieldName + "=?");
 		c.params.add(Long.toString(x));
 		return c;
 	}
-	public Condition<Row, RowOrder> lessThan(long x) {
-		Condition<Row, RowOrder> c = new Condition<Row, RowOrder>(this.tableDefinition, this.fieldName + "<?");
+	public Condition<Row> lessThan(long x) {
+		Condition<Row> c = new Condition<Row>(this.tableDefinition, this.fieldName + "<?");
 		c.params.add(Long.toString(x));
 		return c;
 	}
--- net/examp1e/picoorm/StringPredicate.java
+++ net/examp1e/picoorm/StringPredicate.java
@@ -1,14 +1,14 @@
 package net.examp1e.picoorm;
 
-public class StringPredicate<Row extends AbstractRow, RowOrder> {
-	final TableDefinition<Row, RowOrder> tableDefinition;
+public class StringPredicate<Row extends AbstractRow> {
+	final TableDefinition<Row> tableDefinition;
 	String fieldName;
-	public StringPredicate(TableDefinition<Row, RowOrder> tableDefinition, String fieldName) {
+	public StringPredicate(TableDefinition<Row> tableDefinition, String fieldName) {
 		this.tableDefinition = tableDefinition;
 		this.fieldName = fieldName;
 	}
-	public Condition<Row, RowOrder> is(String x) {
-		Condition<Row, RowOrder> c = new Condition<Row, RowOrder>(this.tableDefinition, this.fieldName + "=?");
+	public Condition<Row> is(String x) {
+		Condition<Row> c = new Condition<Row>(this.tableDefinition, this.fieldName + "=?");
 		c.params.add(x);
 		return c;
 	}
