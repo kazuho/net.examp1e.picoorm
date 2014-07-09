1f265218e9da41536571b8e0f989bb4c43a275d7
Kazuho Oku <kazuhooku@gmail.com>
2014-07-09 13:40:07 +0900

    asc/desc should be a field rather than a method

--- net/examp1e/picoorm/Predicate.java
+++ net/examp1e/picoorm/Predicate.java
@@ -2,25 +2,36 @@ package net.examp1e.picoorm;
 
 public abstract class Predicate<Row extends AbstractRow> {
 	
+	public static class OrderPredicate<Row extends AbstractRow> extends Predicate<Row> {
+		boolean isAsc = true;
+		OrderPredicate(Predicate<Row> src, boolean isAsc) {
+			super(src);
+			this.isAsc = isAsc;
+		}
+		@Override
+		boolean orderIsAscending() {
+			return this.isAsc;
+		}
+	}
+
 	final TableDefinition<Row> tableDefinition;
 	String fieldName;
+
+	public final OrderPredicate<Row> asc;
+	public final OrderPredicate<Row> desc;
 
 	protected Predicate(TableDefinition<Row> tableDefinition, String fieldName) {
 		this.tableDefinition = tableDefinition;
 		this.fieldName = fieldName;
+		this.asc = new OrderPredicate<Row>(this, true);
+		this.desc = new OrderPredicate<Row>(this, false);
 	}
 
 	protected Predicate(Predicate<Row> src) {
 		this.tableDefinition = src.tableDefinition;
 		this.fieldName = src.fieldName;
-	}
-
-	public Predicate<Row> asc() {
-		return new OrderPredicate<Row>(this, true);
-	}
-
-	public Predicate<Row> desc() {
-		return new OrderPredicate<Row>(this, false);
+		this.asc = src.asc;
+		this.desc = src.desc;
 	}
 
 	boolean orderIsAscending() {
