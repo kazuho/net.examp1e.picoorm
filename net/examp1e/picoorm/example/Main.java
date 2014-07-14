9cb40de2f19a75fafe60182c574bdd2da4d704b9
Kazuho Oku <kazuhooku@gmail.com>
2014-07-14 13:23:45 +0900

    add static member `all` refers to "all rows" (note: ALL is an SQL keyword and thus is not never used as a column name)

--- net/examp1e/picoorm/example/Main.java
+++ net/examp1e/picoorm/example/Main.java
@@ -21,6 +21,11 @@ public class Main {
 			new Member().setName("tokuhirom").insert(conn);
 			new Member().setName("kazuho").insert(conn);
 
+			// SELECT FROM member;
+			for (Member m : Member.all.select(conn)) {
+				System.out.println(Long.toString(m.getId()) + ":" + m.getName());
+			}
+
 			// SELECT FROM member WHERE (name=?) OR (name=?)
 			for (Member m : Member.name.is("yappo").or(Member.name.is("tokuhirom")).orderBy(Member.id.asc).select(conn)) {
 				System.out.println(Long.toString(m.getId()) + ":" + m.getName());
